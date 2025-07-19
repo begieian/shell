dbc51463da1899e7367974e864dc42cfac96f80c
begieian <begieianrivera@gmail.com>
2025-07-19 11:51:19 +0800

    adding add-new-local-user

---
+++ add-new-local-user.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# check if they're using root
+
+if [[ ${UID} -ne 0 ]]
+then
+  echo 'You are not using root account. Please try again.'
+  exit 1
+fi
+
+# Check if the argument is provided.
+if [[ ${#} -lt 1 ]]
+then
+  echo "Please provide a username and full name after script."
+  echo "${0} USER_NAME [COMMENT]"
+  exit 1
+fi
+
+# Create new account with the username as first argument and fullname as second argument
+USER_NAME=${1}
+COMMENT=${2}
+
+# Generate password
+RANDOM_CHARACTER=$(echo '!@#$%^&*()-_=+' | fold -w1 | shuf | head -c1)
+RANDOM_PASSWORD=$(date +%s%N | sha256sum | head -c9)
+PASSWORD=${RANDOM_PASSWORD}${RANDOM_CHARACTER}
+
+useradd -c ${COMMENT} -m ${USER_NAME}
+if [[ ${?} -gt 0 ]]
+then
+  echo 'Unable to create a user. Please try again.'
+  exit 1
+fi
+
+echo "${PASSWORD}" | passwd --stdin ${USER_NAME}
+
+if [[ ${?} -gt 0 ]]
+then
+  echo 'Unable to save a password. Please try again.'
+  exit 1
+fi
+
+passwd -e ${USER_NAME}
+
+echo "This is the username: ${USER_NAME}"
+echo "This is the comment: ${COMMENT}"
+echo "This is the password: ${PASSWORD}"
+echo "This is the hostname: ${HOSTNAME}"
