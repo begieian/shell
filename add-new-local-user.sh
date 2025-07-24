46cc8310731857b8a0e1eb7a0268e82d3ba27f73
begieian <begieianrivera@gmail.com>
2025-07-24 12:06:37 +0800

    removed the add-new and changed the add-newer to add-new shellscript

--- add-new-local-user.sh
+++ add-new-local-user.sh
@@ -1,51 +1,65 @@
 #!/bin/bash
 
-# check if they're using root
-
-if [[ ${UID} -ne 0 ]]
+# MAKE SURE THIS IS BEING USED AS ROOT OR SUDO
+if [[ "${UID}" -ne 0 ]]
 then
-  echo 'You are not using root account. Please try again.'
+  echo 'Please use the root account or run this script as sudo.'
   exit 1
 fi
 
-# Check if the argument is provided.
+# MAKE SURE THE USER WILL PROVIDE ARGUMENTS
 if [[ ${#} -lt 1 ]]
 then
-  echo "Please provide a username and full name after script."
-  echo "${0} USER_NAME [COMMENT]"
+  echo 'Please follow this instruction.'
+  echo "Usage: ${0} USER_NAME [COMMENT] ..."
   exit 1
 fi
 
-# Create new account with the username as first argument and fullname as second argument
+
+# TAKING FIRST ARGUMENT AS USERNAME THEN SHIFT THEN TAKING ALL ARGUMENTS AS COMMENT
 USER_NAME=${1}
 shift
 COMMENT=${@}
 
-# Generate password
-RANDOM_CHARACTER=$(echo '!@#$%^&*()-_=+' | fold -w1 | shuf | head -c1)
-RANDOM_PASSWORD=$(date +%s%N | sha256sum | head -c9)
+
+# GENERATING PASSWORD
+RANDOM_CHARACTER=$(echo '!@#$%^&*()_-+=' | fold -w1 | shuf | head -c1)
+RANDOM_PASSWORD=$(date +%s%N${RANDOM}${RANDOM} | sha256sum | head -c9)
 PASSWORD=${RANDOM_PASSWORD}${RANDOM_CHARACTER}
 
+
 useradd -c "${COMMENT}" -m ${USER_NAME}
-if [[ ${?} -ne 0 ]]
+if [[ ${?} -gt 0 ]]
 then
-  echo 'Unable to create a user. Please try again.'
+  echo 'Something went wrong. Please try again.'
   exit 1
 fi
 
 echo "${PASSWORD}" | passwd --stdin ${USER_NAME}
-
-if [[ ${?} -ne 0 ]]
+if [[ ${?} -gt 0 ]]
 then
-  echo 'Unable to save a password. Please try again.'
+  echo 'Something went wrong in password setup. Please try again.'
   exit 1
 fi
 
 passwd -e ${USER_NAME}
 
 
-# Show the details of the newly created account.
-echo "This is the username: ${USER_NAME}"
-echo "This is the comment: ${COMMENT}"
-echo "This is the password: ${PASSWORD}"
-echo "This is the hostname: ${HOSTNAME}"
+FILE="./${USER_NAME}"
+
+echo " ======================" 1>> ${FILE} 2> /dev/null
+echo "| Successfully Created |" 1>> ${FILE} 2> /dev/null
+echo " ======================" 1>> ${FILE} 2> /dev/null
+echo "Username:" 1>> ${FILE} 2> /dev/null
+echo "${USER_NAME}" 1>> ${FILE} 2> /dev/null
+echo 1>> ${FILE} 2> /dev/null
+echo "Comment:" 1>> ${FILE} 2> /dev/null
+echo "${COMMENT}" 1>> ${FILE} 2> /dev/null
+echo 1>> ${FILE} 2> /dev/null
+echo "Password:" 1>> ${FILE} 2> /dev/null
+echo "${PASSWORD}" 1>> ${FILE} 2> /dev/null
+echo 1>> ${FILE} 2> /dev/null
+echo "Hostname:" 1>> ${FILE} 2> /dev/null
+echo "${HOSTNAME}" 1>> ${FILE} 2> /dev/null
+
+
